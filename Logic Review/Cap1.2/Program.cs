498eaab1ce7680e68a8d6fb0860b9d79409693ce
Majulia <majulialcarvalho@gmail.com>
2021-12-13 19:12:03 -0300

    tipos de valores

---
+++ Logic Review/Cap1.2/Program.cs
@@ -0,0 +1,15 @@
+﻿using static System.Console;
+
+namespace Cap1._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            //um caractere -> char -> aspas simples 
+            
+            //uma cadeira -> string -> aspas suplas
+        }
+    }
+}

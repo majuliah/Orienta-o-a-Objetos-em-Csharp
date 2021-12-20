52f3e34c945e8a4759af491835bfabe6e5e37090
Majulia <majulialcarvalho@gmail.com>
2021-12-20 20:09:38 -0300

    Exercício 3, incluindo métodos no algoritmo do triângulo

---
+++ 02 - Classes, Attributes, Methods, Static Members/Cap1.4/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Globalization;
+using static System.Console;
+
+namespace Cap1._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] valores;
+            
+            Triangulo trianguloX, trianguloY;
+            trianguloX = new Triangulo();
+            trianguloY = new Triangulo();
+            double areaX, areaY;
+            
+            WriteLine("Entre com os três valores do triângulo X");
+            valores = ReadLine().Split(' ');
+            trianguloX.ladoA = double.Parse(valores[0], CultureInfo.InvariantCulture);
+            trianguloX.ladoB = double.Parse(valores[1], CultureInfo.InvariantCulture);
+            trianguloX.ladoC = double.Parse(valores[2], CultureInfo.InvariantCulture);
+
+            areaX = trianguloX.CalculoArea();
+            
+                        
+            WriteLine("Entre com os três valores do triângulo Y");
+            valores = ReadLine().Split(' ');
+            trianguloY.ladoA = double.Parse(valores[0], CultureInfo.InvariantCulture);
+            trianguloY.ladoB = double.Parse(valores[1], CultureInfo.InvariantCulture);
+            trianguloY.ladoC = double.Parse(valores[2], CultureInfo.InvariantCulture);
+
+            areaY = trianguloY.CalculoArea();
+
+            if (areaX > areaY)
+                WriteLine($"O maior triângulo é o X com a área de {areaX.ToString("F4", CultureInfo.InvariantCulture)}");
+            else
+                WriteLine($"O maior triângulo é o Y com a área de {areaY.ToString("F4", CultureInfo.InvariantCulture)}");
+            
+        }
+    }
+}
---
+++ 02 - Classes, Attributes, Methods, Static Members/Cap1.4/Triangulo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Cap1._4
+{
+    public class Triangulo
+    {
+        public double ladoA, ladoB, ladoC;
+
+        public double CalculoArea()
+        {
+            double formulaP = (ladoA + ladoB + ladoC) / 2.0;
+            double calculoRaiz = Math.Sqrt(formulaP * (formulaP - ladoA) * (formulaP - ladoB) * (formulaP - ladoC) / 2.0);
+            return calculoRaiz;
+        }
+        
+    }
+}
+
+
+
+
+
+
+
+
+

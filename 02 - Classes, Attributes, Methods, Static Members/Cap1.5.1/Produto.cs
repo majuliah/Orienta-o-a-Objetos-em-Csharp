973b6ff3857f0862ec82aaa8c4e74899944bb7cc
Majulia <majulialcarvalho@gmail.com>
2021-12-23 16:42:18 -0300

    Finalizado o primeiro mini projetinho :D

---
+++ 02 - Classes, Attributes, Methods, Static Members/Cap1.5.1/Produto.cs
@@ -0,0 +1,32 @@
+﻿using System.Globalization;
+
+namespace Cap1._5._1
+{
+    public class Produto
+    {
+        public string Nome;
+        public int Quantidade;
+        public double Valor;
+
+        public double valorTotal()
+        {
+            return Quantidade * Valor;
+        }
+
+        public void Adicionar(int qtde)
+        {
+            Quantidade += qtde;
+        }
+
+        public void Remover(int qtde)
+        {
+            Quantidade -= qtde;
+        }
+
+        public override string ToString()
+        {
+            return (Nome + ", R$" + Valor.ToString("F2", CultureInfo.InvariantCulture) + " em " + Quantidade +
+                    " no estoque, tem o total de R$" + valorTotal().ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
---
+++ 02 - Classes, Attributes, Methods, Static Members/Cap1.5.1/Program.cs
@@ -0,0 +1,46 @@
+﻿using static System.Console;
+using System.Globalization;
+
+namespace Cap1._5._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto produto = new Produto();
+            string[] dadosProduto;
+            
+            WriteLine("Entre com o Nome, Quantidade e Valor do produto: ");
+            dadosProduto = ReadLine().Split(' ');
+            produto.Nome = dadosProduto[0];
+            produto.Quantidade = int.Parse(dadosProduto[1]);
+            produto.Valor = double.Parse(dadosProduto[2], CultureInfo.InvariantCulture);
+            WriteLine($"Dados do produto: {produto.ToString()}");
+            
+            WriteLine($"Você quer ADICIONAR(1) ou REMOVER(2) o produto {produto.Nome} do estoque?");
+            int decisao = int.Parse(ReadLine());
+
+            if (decisao == 1)
+                WriteLine($"Quantas unidades quer adicionar?");
+            else
+                WriteLine($"Quantas unidades quer remover?");
+            
+            int quantidade = int.Parse(ReadLine());
+            
+            switch (decisao)
+            {
+                case 1:
+                    produto.Adicionar(quantidade);
+                    WriteLine($"Dados do produto após adição: {produto.ToString()}");
+                    break;
+                case 2:
+                    produto.Remover(quantidade);
+                    WriteLine($"Dados do produto após remoção: {produto.ToString()}");
+                    break;
+                default:
+                    WriteLine($"Opção inválida. Encerrando o programa");
+                    break;
+            }
+        }
+    }
+}

5016ac43c32f4026bb82a77d5c0303bd3f3a6f96
Majulia <majulialcarvalho@gmail.com>
2021-12-28 00:34:15 -0300

    Terminar Exercicio 3

---
+++ 02 - Classes, Attributes, Methods, Static Members/Cap1.6.2/ALuno.cs
@@ -0,0 +1,15 @@
+﻿namespace Cap1._6._2
+{
+    public class ALuno
+    {
+        public string Nome;
+        public double Nota1;
+        public double Nota2;
+        public double Nota3;
+
+        public double CalculaMedia()
+        {
+            return (Nota1 + Nota2 + Nota3) / 3;
+        }
+    }
+}
---
+++ 02 - Classes, Attributes, Methods, Static Members/Cap1.6.2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Globalization;
+using static System.Console;
+
+namespace Cap1._6._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ALuno aluno = new ALuno();
+            string[] notas;
+            double media, quantoFalta;
+            
+            WriteLine("Entre com o nome e as três notas de um aluno:");
+            notas = ReadLine().Split(' ');
+            aluno.Nome = notas[0];
+            aluno.Nota1 = double.Parse(notas[1], CultureInfo.InvariantCulture);
+            aluno.Nota2 = double.Parse(notas[2], CultureInfo.InvariantCulture);
+            aluno.Nota3 = double.Parse(notas[3], CultureInfo.InvariantCulture);
+
+            media = aluno.CalculaMedia();
+
+            if (media >= 60.0)
+                WriteLine($"{aluno.Nome} foi APROVADO COM MEDIA {media.ToString("F2", CultureInfo.InvariantCulture)}");
+            else
+            {
+                quantoFalta = (60.0 - media);
+                WriteLine($"{aluno.Nome} REPROVADO. Faltavam {quantoFalta.ToString("F2", CultureInfo.InvariantCulture)} para passar.");
+            }
+
+        }
+    }
+}

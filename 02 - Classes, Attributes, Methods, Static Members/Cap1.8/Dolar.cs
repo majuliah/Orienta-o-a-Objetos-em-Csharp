9c041f405c4e4f8be1319a672f0e2a536b601724
Majulia <majulialcarvalho@gmail.com>
2022-01-04 14:04:17 -0300

    Desafio Dólar (okay)

---
+++ 02 - Classes, Attributes, Methods, Static Members/Cap1.8/Dolar.cs
@@ -0,0 +1,13 @@
+﻿namespace Cap1._8
+{
+    public class Dolar
+    {
+
+        public double ConverterDolarEmReal(double valorEmReais)
+        {
+            double valorDolar = valorEmReais * 5.45;
+            double valorTotal = valorDolar + (valorDolar * 0.6);
+            return valorTotal;
+        }
+    }
+}
---
+++ 02 - Classes, Attributes, Methods, Static Members/Cap1.8/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Globalization;
+using static System.Console;
+
+namespace Cap1._8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dolar calcularDolar = new Dolar();
+            WriteLine("Entre com o valor em reais para converter para dólar: ");
+            double valor = double.Parse(ReadLine(), CultureInfo.InvariantCulture);
+
+            double resultado = calcularDolar.ConverterDolarEmReal(valor);
+            WriteLine(resultado);
+
+        }
+    }
+}

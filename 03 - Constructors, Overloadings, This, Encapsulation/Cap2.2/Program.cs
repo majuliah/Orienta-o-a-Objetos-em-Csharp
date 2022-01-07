c0bc9338112a7c1d9d2693cb395bf7fe8ee58fc4
Majulia <majulialcarvalho@gmail.com>
2022-01-07 14:19:23 -0300

    iniciando encapsulamento

---
+++ 03 - Constructors, Overloadings, This, Encapsulation/Cap2.2/Program.cs
@@ -0,0 +1,12 @@
+﻿using static System.Console;
+
+namespace Cap2._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Hello World!");
+        }
+    }
+}

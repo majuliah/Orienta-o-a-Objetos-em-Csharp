27f212e2a60786f4bf7f0a88d9571e65ed92a643
Majulia <majulialcarvalho@gmail.com>
2022-01-08 18:39:21 -0300

    Teste

--- 03 - Constructors, Overloadings, This, Encapsulation/Cap2.3/Program.cs
+++ 03 - Constructors, Overloadings, This, Encapsulation/Cap2.3/Program.cs
@@ -9,8 +9,18 @@ namespace Cap2._3
             string[] infosConta;
             WriteLine("Entre com o Nome, o número da conta com 6 dígitos e o valor que quer depositar.");
             infosConta = ReadLine().Split(' ');
-            Conta newConta = new Conta(infosConta[0], infosConta[1], double.Parse(infosConta[2]));
+            
 
+            if (infosConta.Length == 3)
+            {
+                Conta newConta = new Conta(infosConta[0], infosConta[1], double.Parse(infosConta[2]));    
+            }
+            else
+            {
+                
+            }
+
+            
             WriteLine(" ");
             WriteLine("Você quer fazer um depósito ou uma ");
             

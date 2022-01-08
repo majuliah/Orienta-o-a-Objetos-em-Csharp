867c77e936db8b859f0d5ae36112bf01739af302
Majulia <majulialcarvalho@gmail.com>
2022-01-08 18:26:45 -0300

    Correção das aulas 61 - 62

---
+++ 03 - Constructors, Overloadings, This, Encapsulation/Cap2.4/Program.cs
@@ -0,0 +1,13 @@
+﻿using static System.Console;
+
+namespace Cap2._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Correção aula 61 - 62");
+            
+        }
+    }
+}

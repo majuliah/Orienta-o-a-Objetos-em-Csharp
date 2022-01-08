a3d33b59fef81d2e233878f0b72f82d3bf6615e1
Majulia <majulialcarvalho@gmail.com>
2022-01-08 18:08:45 -0300

    Tentando Reolver o Exercício

---
+++ 03 - Constructors, Overloadings, This, Encapsulation/Cap2.3/Conta.cs
@@ -0,0 +1,40 @@
+﻿namespace Cap2._3
+{
+    public class Conta
+    {
+        private string _nome;
+        public string NumeroConta { get; private set; }
+        public double ValorNaConta { get; private set; }
+
+        public Conta()
+        {
+            ValorNaConta = 0;
+        }
+        
+        public Conta(string nome, string numeroConta, double valorNaConta) : this()
+        {
+            nome = _nome;
+            numeroConta = NumeroConta;
+        }
+        public string Nome
+        {
+             get { return _nome; }
+             set
+             {
+                 if (value != null && value.Length > 2)
+                     _nome = value;
+             }
+        }
+        public void ValorSaque(double valorRetirado)
+        {
+            ValorNaConta -=  valorRetirado - 5.0;
+        }
+        
+        public void ValorDeposito(double valorAdicionado)
+        {
+            ValorNaConta += valorAdicionado;
+        }
+        
+        
+    }
+}
---
+++ 03 - Constructors, Overloadings, This, Encapsulation/Cap2.3/Program.cs
@@ -0,0 +1,22 @@
+﻿using static System.Console;
+
+namespace Cap2._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            WriteLine("Entre com o Nome, o número da conta com 6 dígitos e o valor que quer depositar.");
+            Conta newConta = new Conta();
+            newConta.Nome = ReadLine();
+            newConta.NumeroConta() = ReadLine();
+
+
+
+
+
+
+        }
+    }
+}

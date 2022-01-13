b261b42c443719476bd3ba8932d9f0314610055f
Majulia <majulialcarvalho@gmail.com>
2022-01-13 17:10:30 -0300

    Mini projeto pronto :)

---
+++ 03 - Constructors, Overloadings, This, Encapsulation/Cap2.4/ContaBanco.cs
@@ -0,0 +1,39 @@
+﻿using System.Globalization;
+
+namespace Cap2._4
+{
+    public class ContaBanco
+    {
+        public string NomeTitular { get; private set; }
+        public string NumeroDaConta { get; private set; }
+        public double SaldoConta { get; private set; }
+
+
+        public ContaBanco(string nomeTitular, string numeroDaConta)
+        {
+            NomeTitular = nomeTitular;
+            NumeroDaConta = numeroDaConta;
+        }
+
+        public ContaBanco(string nomeTitular, string numeroDaConta, double saldoConta): this(nomeTitular, numeroDaConta)
+        {
+            SaldoConta = saldoConta;
+        }
+
+        public void Saque(double valorSaque)
+        {
+            SaldoConta -= valorSaque + 1.5;
+        }
+
+        public void Deposito(double valorDeposito)
+        {
+            SaldoConta += valorDeposito;
+        }
+        
+        public override string ToString()
+        {
+            return ($"{NomeTitular}, aqui estão suas informações: \n " +
+                    $"Saldo Atual: {SaldoConta.ToString("F2", CultureInfo.InvariantCulture)} \n");
+        }
+    }
+}
--- 03 - Constructors, Overloadings, This, Encapsulation/Cap2.4/Program.cs
+++ 03 - Constructors, Overloadings, This, Encapsulation/Cap2.4/Program.cs
@@ -1,4 +1,5 @@
-﻿using static System.Console;
+﻿using System.Globalization;
+using static System.Console;
 
 namespace Cap2._4
 {
@@ -6,7 +7,55 @@ namespace Cap2._4
     {
         static void Main(string[] args)
         {
-            WriteLine("Correção aula 61 - 62");
+            int resposta = 0;
+            string[] criandoAconta;
+            ContaBanco usuario;
+            
+            do
+            {
+                WriteLine("BEM VINDO AO BANCO MARAJUBA :D ");
+                WriteLine("PARA CRIAR UMA CONTA, PRESSIONE 5: ");
+                resposta = int.Parse(ReadLine());
+
+            } while (resposta != 5);
+            
+            WriteLine(" ÓTIMO TER VOCÊ COM A GENTE! ");
+            WriteLine(" Você agora precisa entrar com seu nome, um número de conta e se quiser fazer um saque inicial, informar o valor!");
+            criandoAconta = ReadLine().Split(' ');
+
+            
+            if (criandoAconta.Length == 3)
+                usuario = new ContaBanco
+                    (criandoAconta[0], criandoAconta[1], 
+                        double.Parse(criandoAconta[2], CultureInfo.InvariantCulture));
+            else
+                usuario = new ContaBanco
+                    (criandoAconta[0], criandoAconta[1]);
+            
+            WriteLine(usuario);
+
+            while (resposta != 0)
+            {
+                WriteLine($"Você deseja fazer alguma operação?");
+                WriteLine($"([1] SAQUE [2] DEPOSITO [0] ENCERRA)");
+                resposta = int.Parse(ReadLine());
+
+                if (resposta == 1)
+                {
+                    WriteLine($"Informe qual o valor deseja sacar :)");
+                    double sacar = double.Parse(ReadLine(), CultureInfo.InvariantCulture);
+                    usuario.Saque(sacar);
+                }else if (resposta == 2)
+                {
+                    WriteLine($"Informe qual o valor deseja depositar :)");
+                    double depositar = double.Parse(ReadLine(), CultureInfo.InvariantCulture);
+                    usuario.Deposito(depositar);
+                }
+                else
+                    WriteLine($"Encerrando a operação. Foi ótimo poder atender você!");
+                
+                WriteLine(usuario);
+            }
             
         }
     }

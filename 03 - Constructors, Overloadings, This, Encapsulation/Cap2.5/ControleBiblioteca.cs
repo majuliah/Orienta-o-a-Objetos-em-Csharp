b0c002e0b80cf12c16a1f5ae886c4624c742871b
Majulia <majulialcarvalho@gmail.com>
2022-01-16 16:36:40 -0300

    It can be better

--- 03 - Constructors, Overloadings, This, Encapsulation/Cap2.5/ControleBiblioteca.cs
+++ 03 - Constructors, Overloadings, This, Encapsulation/Cap2.5/ControleBiblioteca.cs
@@ -1,7 +1,27 @@
-﻿namespace Cap2._5
+﻿using System.Globalization;
+
+namespace Cap2._5
 {
     public class ControleBiblioteca
     {
-        
+        public string Nome { get; set; }
+        public int Quantidade { get; set; }
+        public double Valor { get; private set; }
+
+        public ControleBiblioteca(string nome, int quantidade)
+        {
+            Nome = nome;
+            Quantidade = quantidade;
+            Valor = 10.20;
+        }
+        public double ValorCompraOuVenda()
+        {
+            return Valor * Quantidade;
+        }
+
+        public override string ToString()
+        {
+            return ($"Você adquiriu o livro {Nome}, em {Quantidade} e deu o total de R${ValorCompraOuVenda().ToString("F2", CultureInfo.InvariantCulture)}");
+        }
     }
 }
--- 03 - Constructors, Overloadings, This, Encapsulation/Cap2.5/Program.cs
+++ 03 - Constructors, Overloadings, This, Encapsulation/Cap2.5/Program.cs
@@ -1,4 +1,5 @@
-﻿using static System.Console;
+﻿using System.Globalization;
+using static System.Console;
 
 namespace Cap2._5
 {
@@ -6,16 +7,49 @@ namespace Cap2._5
     {
         static void Main(string[] args)
         {
-            WriteLine("Bem Vinde à Majúlia's Biblioteca :D ");
-            WriteLine("Em nosso sistema, hoje em dia, podemos realizar as seguintes operações virtualmente, selecione a desejada: ");
-            WriteLine("* [0] SAIR               *");
-            WriteLine("* [1] COMPRAR UM LIVRO   *");
-            WriteLine("* [2] ALUGAR UM LIVRO    *");
-            WriteLine("* [3] VENDER UM LIVRO    *");
-            WriteLine("* [4] CONSULTAR UM LIVRO *");
+            ControleBiblioteca livros;
+            string[] infoLivro;
+            string[] listaDeLivros = new string[5] { "O Extraordinario", "O Conto da Aia", "A Revolução dos Bichos", "Fahrenheit 451", " Admirável Mundo Novo"};
+            int resposta = 9;
+
+            while (resposta != 0)
+            {
+                WriteLine("Bem Vindo à Majúlia's Biblioteca :D ");
+                WriteLine("Em nosso sistema, hoje em dia, podemos realizar as seguintes operações virtualmente, selecione a desejada: ");
+                WriteLine("* [0] SAIR               *");
+                WriteLine("* [1] COMPRAR UM LIVRO   *");
+                WriteLine("* [2] VENDER UM LIVRO    *");
+                WriteLine("* [3] CONSULTAR UM LIVRO *");
+                resposta = int.Parse(ReadLine());
+
+                switch (resposta)
+                {
+                    case 1:
+                        WriteLine($"Você escolheu comprar um livro! As opções são: ");
+                        for (int i = 0; i < 5; i++)
+                            Write(listaDeLivros[i] + ", ");
+                        WriteLine(" \n Escreva o nome do Livro que quer comprar e a quantidade");
+                        infoLivro = ReadLine().Split(' ');
+                        livros = new ControleBiblioteca(infoLivro[0], int.Parse(infoLivro[1]));
+                        WriteLine(livros);
+                        break;
+                    case 2:
+                        
+                        WriteLine($"Você escolheu vender um livro! :) ");
+                        WriteLine("Escreva o nome do Livro que quer vender e a quantidade");
+                        infoLivro = ReadLine().Split(' ');
+                        livros = new ControleBiblioteca(infoLivro[0], int.Parse(infoLivro[1]));
+                        WriteLine(livros);
+                        break;
+                    
+                    case 3:
+                        for (int i = 0; i < 5; i++)
+                            Write(listaDeLivros[i] + " ");
+                        break;
+                }
+            }
             
-            
-            
+            WriteLine($"Volte sempre!!!");
             
         }
     }

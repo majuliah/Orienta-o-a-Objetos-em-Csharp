38a63023e42c72ef72a5b1ade41a12d5224c3f65
Majulia <majulialcarvalho@gmail.com>
2022-01-31 18:30:56 -0300

    lists part 2

---
+++ 04 - Memory, Lists and Arrays/Cap3.6/Program.cs
@@ -0,0 +1,20 @@
+﻿using static System.Console;
+using System.Collections.Generic;
+
+namespace Cap3._6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Lists");
+            
+            List<string> myFirstList = new List<string>();
+            List<string> otherList;
+            List<string> namesLists = new List<string> { "Maria", "Alex", "Maramusa" };
+            
+            
+
+        }
+    }
+}

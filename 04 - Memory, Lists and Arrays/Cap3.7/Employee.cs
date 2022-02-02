a4fc290a0fca23b205f87f9c9d2f6bd1aee447d4
Majulia <majulialcarvalho@gmail.com>
2022-02-01 21:36:27 -0300

    testing knowledge

---
+++ 04 - Memory, Lists and Arrays/Cap3.7/Employee.cs
@@ -0,0 +1,23 @@
+﻿namespace Cap3._7
+{
+    public class Employee
+    {
+        public string EmployeeName { get; set; }
+        public string EmployeeId { get; set; }
+        public double EmployeeSalary { get; private set; }
+
+        public Employee(string employeeName, string employeeId, double employeeSalary)
+        {
+            this.EmployeeName = employeeName;
+            this.EmployeeId = employeeId;
+            this.EmployeeSalary = employeeSalary;
+        }
+
+        public void AdjustingSalary(double quantity)
+        {
+            EmployeeSalary += EmployeeSalary * (quantity / 100.0);
+        }
+        
+        
+    }
+}
---
+++ 04 - Memory, Lists and Arrays/Cap3.7/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Globalization;
+using static System.Console;
+
+namespace Cap3._7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("First Exercice with Lists \n \n");
+
+            WriteLine($"How many employees will be registered?");
+            int quantityOfEmployees = int.Parse(ReadLine());
+
+            List<Employee> employeeRegister = new List<Employee>();
+            for (int i = 0; i < quantityOfEmployees; i++)
+            {
+                WriteLine($"Employee #{i + 1}");
+                string name = ReadLine();
+                string id = ReadLine();
+                double salary = double.Parse(ReadLine(), CultureInfo.InvariantCulture);
+                employeeRegister.Add(new Employee(name, id, salary));
+                //we'll add an object at the list, and the object needs to have a name, an id and a salary :)
+            }
+            
+            WriteLine($"Type the employee ID you want to encrease the salary: ");
+            string employeeId = ReadLine();
+            
+            
+
+
+
+        }
+    }
+}

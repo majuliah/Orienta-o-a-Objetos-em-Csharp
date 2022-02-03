7ee24e1ddeea36fa63c3b733e4327efab0559223
Majulia <majulialcarvalho@gmail.com>
2022-02-03 17:50:53 -0300

    exercicing matrix again

--- 04 - Memory, Lists and Arrays/Cap3.10/Program.cs
+++ 04 - Memory, Lists and Arrays/Cap3.10/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿using static System.Console;
 
 namespace Cap3._10
 {
@@ -6,7 +6,37 @@ namespace Cap3._10
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            WriteLine("Working with Matrix! \n \n");
+
+            WriteLine($"Enter with the dimension of the matrix");
+            int dimensionOfMatrix = int.Parse(ReadLine());
+            double[,] myFirstMatrixThisYear = new double[dimensionOfMatrix,dimensionOfMatrix];
+
+            for (int i = 0; i < dimensionOfMatrix; i++)
+            { 
+                string[] positionsMatrix = ReadLine().Split(' ');
+                for (int j = 0; j < dimensionOfMatrix; j++)
+                {
+                    myFirstMatrixThisYear[i, j] = double.Parse(positionsMatrix[j]);
+                }
+            }
+            
+            WriteLine($"Diagonal: ");
+            for (int i = 0; i < dimensionOfMatrix; i++)
+                WriteLine(myFirstMatrixThisYear[i,i]);
+                
+
+            //arrumar a impressão
+            for (int i = 0; i < dimensionOfMatrix; i++)
+            {
+                for (int j = 0; j < dimensionOfMatrix; j++)
+                {
+                    WriteLine(myFirstMatrixThisYear[i,j] + ' ');
+                }
+            }
+            
+            //fazer o contador de números ímpares, números pares, números positivos e negativos
+            //fazer o desafio do nelio alves
         }
     }
 }

d4f6a693dd7a8d94e214674a0f6710f8e6a3fce2
Majulia <majulialcarvalho@gmail.com>
2022-02-05 15:50:05 -0300

    testing lists

---
+++ 04 - Memory, Lists and Arrays/Cap3.11/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Cap3._11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

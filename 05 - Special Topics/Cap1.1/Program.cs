d85928c6f468ae5fbf90aaeda3491bbd6b2cf8d4
Majulia <majulialcarvalho@gmail.com>
2022-02-10 18:37:35 -0300

    if ternary

---
+++ 05 - Special Topics/Cap1.1/Program.cs
@@ -0,0 +1,21 @@
+﻿using static System.Console;
+
+namespace Cap1._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Simple if and else: Ternary");
+
+            double salary = 20000.0;
+            
+            double discount = (salary < 1000.0 ) ? 0.1 : 0.5;
+
+            double result = salary += salary * discount;
+            
+            Write($" Salary is: {result.ToString()}");
+            
+        }
+    }
+}

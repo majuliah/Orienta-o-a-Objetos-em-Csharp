c58ea0dcdbdf09f5ffef033beeaa5a64d5686e42
Majulia <majulialcarvalho@gmail.com>
2022-02-11 20:46:32 -0300

    DateTime

--- 05 - Special Topics/Cap1.2/Program.cs
+++ 05 - Special Topics/Cap1.2/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using static System.Console;
 
 namespace Cap1._2
 {
@@ -6,7 +7,29 @@ namespace Cap1._2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            WriteLine("DateTime = struct -> value type");
+            //O DateTime armazena Ticks. Os ticks são nanosegundos, como um hashcode
+            //Que quando convertidos, geram a data atual exata. Ele começa a contar desde 01/01/01.  Exemplo: 
+            
+            DateTime dayOne = DateTime.Now;
+            WriteLine(dayOne);
+            WriteLine(dayOne.Ticks);
+            
+            DateTime dayTwo = DateTime.Today;
+            WriteLine(dayTwo);
+
+            DateTime dayThree = DateTime(2022, 02, 11);
+            WriteLine(dayThree);
+            
+            DateTime dayFour = DateTime.Today;
+            WriteLine(dayFour);
+
+            DateTime dayFive = DateTime.UtcNow;
+            WriteLine(dayFive);
+
+            DateTime daySix = DateTime.Parse($"2022-02-11");
+            WriteLine(daySix);
+
         }
     }
 }

1dbade4fd7dfbb71f6abd9af24f3cf3dbfbc95f3
Majulia <majulialcarvalho@gmail.com>
2022-02-12 21:09:16 -0300

    Operations and Properties with TimeSpan

---
+++ 05 - Special Topics/Cap1.5/Program.cs
@@ -0,0 +1,13 @@
+﻿using static System.Console;
+
+namespace Cap1._5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Properties and operations with TimeSPan");
+            
+        }
+    }
+}

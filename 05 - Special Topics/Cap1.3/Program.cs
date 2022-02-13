d180fb2621e2d7d75fa9cab39a93e5f742b9f6f5
Majulia <majulialcarvalho@gmail.com>
2022-02-12 21:01:19 -0300

    DateTime Functions

--- 05 - Special Topics/Cap1.3/Program.cs
+++ 05 - Special Topics/Cap1.3/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using static System.Console;
 
 namespace Cap1._3
 {
@@ -6,7 +7,41 @@ namespace Cap1._3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            WriteLine("TimeSpan \n -------- \n");
+            //a struct that represents a duration
+            //with ticks.
+
+            TimeSpan timeOne = new TimeSpan(12, 35, 48 );
+            WriteLine(timeOne);
+            WriteLine(timeOne.Ticks);
+
+            TimeSpan timeTwo = new TimeSpan();
+            WriteLine(timeTwo);
+
+            TimeSpan timeThree = new TimeSpan(900000000L);
+            WriteLine(timeThree);
+
+            TimeSpan timeFour = new TimeSpan(2, 14, 25, 16);
+            WriteLine(timeFour);
+
+            TimeSpan timeFive = new TimeSpan(5, 13, 14, 25, 35);
+            WriteLine(timeFive);
+
+            TimeSpan timeSix = TimeSpan.FromDays(2.5);
+            WriteLine(timeSix);
+            //allows conversions by days, hours, months, etc to hours
+
+            TimeSpan timeSeven = TimeSpan.FromHours(12.7);
+            WriteLine(timeSeven);
+
+            TimeSpan timeEight = TimeSpan.FromMilliseconds(568);
+            WriteLine(timeEight);
+            
+            
+
+
+
+
         }
     }
 }
---
+++ 05 - Special Topics/Cap1.4/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using static System.Console;
+
+namespace Cap1._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Properties with DateTime:  \n");
+
+            DateTime dateOne = new DateTime(2001, 12, 26, 08, 15, 50, 587);
+            WriteLine(dateOne);
+            WriteLine($"Date: " + dateOne.Date);
+            WriteLine($"Day: " + dateOne.Day);
+            WriteLine($"Day of Week: " + dateOne.DayOfWeek);
+            WriteLine($"Day of Year: " + dateOne.DayOfYear);
+            WriteLine($"Hour: " + dateOne.Hour);
+            WriteLine($"Kind: " + dateOne.Kind);
+            WriteLine($"Millisecond: " + dateOne.Millisecond);
+            WriteLine($"Minute: " + dateOne.Minute);
+            WriteLine($"Month: " + dateOne.Month);
+            WriteLine($"Second: " + dateOne.Second);
+            WriteLine($"Ticks: " + dateOne.Ticks);
+            WriteLine($"Time Of Day: " + dateOne.TimeOfDay);
+            WriteLine($"Year: " + dateOne.Year);
+            
+            
+            WriteLine(dateOne.ToLongDateString());
+            string stringStorage = dateOne.ToLongDateString();
+            WriteLine(stringStorage);
+            
+            WriteLine(dateOne.ToLongTimeString());
+            WriteLine(dateOne.ToBinary());
+            WriteLine(dateOne.ToFileTime());
+            WriteLine(dateOne.ToShortDateString());
+            WriteLine(dateOne.ToShortTimeString());
+            WriteLine(dateOne.ToString($"yyyy-MM-dd HH:mm:ss"));
+            WriteLine(dateOne.ToString($"yyyy-MM-dd HH:mm:ss.fff"));
+
+
+            DateTime dateTwo = dateOne.AddHours(2);
+            WriteLine($"Adding 2 hours to {dateOne} = {dateTwo}");
+            
+            DateTime dateThree = dateOne.AddMonths(5);
+            WriteLine($"Adding 5 month to {dateOne} = {dateThree}");
+            
+        }
+    }
+}

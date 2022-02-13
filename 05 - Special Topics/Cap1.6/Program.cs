a02c7f722f203dd4de4306f8fb8eccf880eec4c9
Majulia <majulialcarvalho@gmail.com>
2022-02-12 22:09:12 -0300

    date format and utc and local

---
+++ 05 - Special Topics/Cap1.6/Program.cs
@@ -0,0 +1,18 @@
+﻿using static System.Console;
+
+namespace Cap1._6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("DateTimeKind: \n");
+            
+            //do the examples :o
+            
+            
+            
+            
+        }
+    }
+}

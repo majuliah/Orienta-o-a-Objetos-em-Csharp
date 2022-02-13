5c52c6872e2d1b4d8c756c03077d994379c87244
Majulia <majulialcarvalho@gmail.com>
2022-02-12 21:38:10 -0300

    OperationsTimeSpan

--- 05 - Special Topics/Cap1.5/Program.cs
+++ 05 - Special Topics/Cap1.5/Program.cs
@@ -1,4 +1,5 @@
-﻿using static System.Console;
+﻿using System;
+using static System.Console;
 
 namespace Cap1._5
 {
@@ -8,6 +9,46 @@ namespace Cap1._5
         {
             WriteLine("Properties and operations with TimeSPan");
             
+            TimeSpan timeOne = TimeSpan.MaxValue;
+            WriteLine(timeOne);
+            
+            TimeSpan timeTwo = TimeSpan.MinValue;
+            WriteLine(timeTwo);
+            
+            TimeSpan timeThree = TimeSpan.Zero;
+            WriteLine(timeThree);
+
+            TimeSpan timeFour = new TimeSpan(2, 3, 4, 5, 6);
+            WriteLine(timeFour.Days);
+            WriteLine(timeFour.Hours);
+            WriteLine(timeFour.Minutes);
+            WriteLine(timeFour.Milliseconds);
+            WriteLine(timeFour.Seconds);
+            WriteLine(timeFour.Ticks);
+            
+            WriteLine(timeFour.TotalDays);
+            WriteLine(timeFour.TotalHours);
+            WriteLine(timeFour.TotalMilliseconds);
+            WriteLine(timeFour.TotalMinutes);
+            WriteLine(timeFour.TotalSeconds);
+
+
+            TimeSpan timeFive = new TimeSpan(1, 30, 10);
+            TimeSpan timeSix = new TimeSpan(0, 10, 5);
+
+            TimeSpan sum = timeFive.Add(timeSix);
+            WriteLine(sum);
+
+            TimeSpan sub = timeFive.Subtract(timeSix);
+            WriteLine(sub);
+
+            TimeSpan mult = timeFive.Multiply(2.0);
+            WriteLine(mult);
+
+            TimeSpan div = timeFive.Divide(1.5);
+            WriteLine(div);
+            
+
         }
     }
 }

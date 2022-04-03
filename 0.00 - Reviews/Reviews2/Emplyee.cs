c72169e686c9484b0e665841938c8202fc311a35
Majulia <majulialcarvalho@gmail.com>
2022-04-03 19:40:51 -0300

    review3

---
+++ 0.00 - Reviews/Reviews2/Emplyee.cs
@@ -0,0 +1,22 @@
+﻿namespace Reviews2
+{
+    public class Emplyee
+    {
+        private string _name;  
+        public double Salary { get; private set; }
+        public int Age { get; set; }
+
+        public string SetName
+        {
+            get { return _name; }
+            set
+            {
+                if (value != null || value.Length > 3)
+                {
+                    _name = value;    
+                }
+                
+            }
+        }
+    }
+}
--- 0.00 - Reviews/Reviews2/Program.cs
+++ 0.00 - Reviews/Reviews2/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Reviews2
 {
@@ -7,6 +8,20 @@ namespace Reviews2
         static void Main(string[] args)
         {
             Console.WriteLine("Encapsulation");
+
+            double? x = 10;
+            double? y = x ?? 5;
+
+            List<string> nomes = new List<string>(70);
+            //para fazer uma lista que recebe dados do banco de dados, deve ser só colocar os tipos do valor, exemplo
+            //fazer uma lista com os campos nome, ou uma lista com todos os campos, ainda não sei, veremos heh heh hehh
+
+            foreach (var name in nomes)  
+            {
+                Console.WriteLine(name);    
+            }
+            
+            
         }
     }
 }

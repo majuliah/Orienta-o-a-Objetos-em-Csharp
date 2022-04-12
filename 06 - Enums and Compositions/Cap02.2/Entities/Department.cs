572c1a734e8f868794c6225e2e34c7fb5c70fc19
Majulia <majulialcarvalho@gmail.com>
2022-04-12 20:42:15 -0300

    part 2 ready go fight

---
+++ 06 - Enums and Compositions/Cap02.2/Entities/Department.cs
@@ -0,0 +1,14 @@
+﻿namespace Cap02._2.Entities
+{
+    public class Department
+    {
+        public string Name { get; set; }
+
+        public Department() { }
+
+        public Department(string name)
+        {
+            Name = name;
+        }
+    }
+}
---
+++ 06 - Enums and Compositions/Cap02.2/Entities/Enums/EmployeeLevel.cs
@@ -0,0 +1,9 @@
+﻿namespace Cap02._2.Entities.Enums
+{
+    public enum EmployeeLevel
+    {
+        Junior = 0,
+        Medium = 1,
+        Senior = 2
+    }
+}

0a0df5953726ebea2a843754ccfd2c8ae1694eed
Majulia <majulialcarvalho@gmail.com>
2022-05-09 07:42:38 -0300

    putting thing together

--- 07 - Heritage and Polymorphism/Cap02.02/Program.cs
+++ 07 - Heritage and Polymorphism/Cap02.02/Program.cs
@@ -7,8 +7,9 @@ namespace Cap02._02
     {
         public static void Main(string[] args)
         {
-            WriteLine("Beginning Heritage and Polymorphism");
-            WriteLine("");
+            WriteLine("Part two of Heritage and Polymorphism: ");
+            WriteLine(" Up and Downcasting ");
+            
             BusinessAccount account = new BusinessAccount(8010, "Bob Brown", 100.0, 500.0);
 
             WriteLine(account.Balance);

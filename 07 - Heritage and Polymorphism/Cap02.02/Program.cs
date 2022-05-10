5c17726140aeab2035872597593002fe8d37a017
Majulia <majulialcarvalho@gmail.com>
2022-05-10 08:51:16 -0300

    Upcasting

--- 07 - Heritage and Polymorphism/Cap02.02/Program.cs
+++ 07 - Heritage and Polymorphism/Cap02.02/Program.cs
@@ -10,10 +10,27 @@ namespace Cap02._02
             WriteLine(" Part two of Heritage and Polymorphism: ");
             WriteLine(" Up and Downcasting ");
             
-            BusinessAccount account = new BusinessAccount(8010, "Bob Brown", 100.0, 500.0);
+            Account count1 = new Account(1001, "Alex", 0.0);
+            BusinessAccount business1 = new BusinessAccount(2002, "Majulha", 500.00, 700.00);
 
-            WriteLine(account.Balance);
+            //UPCASTING:converts a subclass to a superclass
+            Account account1 = business1;
+            //não precisa instanciar porque ela está recebendo um obj já pronto.
+            Account count2 = new BusinessAccount(3002, "Maraju", 50.00, 800.00);
+            //Podemos atribuir uma subclasse pra uma variável do tipo superclasse :)
+        
+            Account count3 = new SavingAccounts(1008, "MariJu", 0.0, 0.01);
+            //Qualquer subclasse que herda de Account :)
+
+                    //superclass
+            Account conta9 = new BusinessAccount(7006, "Maramusa", 70.00, 900.00);
+                                 //subclass
             
+
+            //DOWNCASTING
+        
+        
+        
         }
     }
 }

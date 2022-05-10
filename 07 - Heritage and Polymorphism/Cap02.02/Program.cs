f6a0fa181fca5ac045fe74fce4692c3883098363
Majulia <majulialcarvalho@gmail.com>
2022-05-10 09:16:57 -0300

    This one in particular not suposed to work

--- 07 - Heritage and Polymorphism/Cap02.02/Program.cs
+++ 07 - Heritage and Polymorphism/Cap02.02/Program.cs
@@ -28,6 +28,89 @@ namespace Cap02._02
             
 
             //DOWNCASTING
+                                     //var count2 é uma variável do tipo do businessccount MAS está armazenada em uma var do tipo Account.
+            BusinessAccount count4 = count2;
+                //vai dar erro porque estamos tentando atribuir uma superclasse pra uma subclasse, e isso não pode ser feito implícitamente.
+
+            //para converter o count 2 para o tipo Business, é preciso fazer a conversão explícita
+
+            BusinessAccount count5 = (BusinessAccount) count2;
+            //Agora a var count5 tem os atributos do businessaccount
+            count5.Loan(100.00);
+                  //see?
+
+            count2.Loan();
+                  //erro porque: Mesmo sendo instanciada como businessAccount, a variavel count2 é do tipo Account, e o Loan é propriedade do businessAccount
+                    //Mesmo instanciado como BusinessAccount, o compilador não sabe que ele é desse tipo por ter sido atribuído a um tipo Account.
+                    //Só funcionaria se fizéssemos o downcasting
+
+            //Part of tests:
+
+            //Downcasting é uma operação não segura porque 
+            //as vezes pode ser executada de forma errada e o compilador só percebe em tempo de compilação.
+            //Exemplo:
+             BusinessAccount account6 = (BusinessAccount) conta9;
+             //Na hora de compilar vai dar erro, porque a variável 9 é do tipo SavingAccount. E não podemos 
+             //converter uma subclasse para outra
+
+            //PARA TESTAR SE UM OBJ É DE UM TIPO OU DE OUTRO
+
+                if(conta9 is BusinessAccount)
+                {
+                    BusinessAccount conta6 = (BusinessAccount)conta9;
+                    conta6.Loan(100.00);
+                    WriteLine("Loan executed!!");
+                }
+
+                if(conta9 is SavingAccounts)
+                {
+                    SavingAccounts conta6 = (SavingAccounts)conta9;
+                    // a palavra as substitui este casting:
+                    SavingAccounts conta7 = conta9 as SavingAccounts;
+                    conta6.UpdateBalance();
+                    WriteLine("Updated:) ");
+                }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         
         
         

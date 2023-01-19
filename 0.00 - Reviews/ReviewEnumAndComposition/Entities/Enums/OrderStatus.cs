35dbad2cf68685ccfe5f2012e67a14e016b818b7
majulha <majulialcarvalho@gmail.com>
2023-01-19 14:57:36 -0300

    organização de códigos

---
+++ 0.00 - Reviews/ReviewEnumAndComposition/Entities/Enums/OrderStatus.cs
@@ -0,0 +1,10 @@
+﻿namespace ReviewEnumAndComposition.Entities.Enums
+{
+    public enum OrderStatus : int
+    {
+        PendingPayment = 0,
+        Processing = 1,
+        Sending = 2,
+        Delivered = 3
+    }
+}
---
+++ 0.00 - Reviews/ReviewEnumAndComposition/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using static System.Console;
+using ReviewEnumAndComposition.Entities;
+using ReviewEnumAndComposition.Entities.Enums;
+
+
+namespace ReviewEnumAndComposition
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Hello World!");
+
+            Order primeiropedido = new Order {Id = 129083, Moment = DateTime.Today, Status = OrderStatus.Delivered};
+            WriteLine(primeiropedido);
+            string statusOrder = OrderStatus.Processing.ToString();
+            WriteLine(statusOrder);
+
+            OrderStatus pedido = Enum.Parse<OrderStatus>("Delivered");
+            WriteLine(pedido);
+
+
+        }
+    }
+}

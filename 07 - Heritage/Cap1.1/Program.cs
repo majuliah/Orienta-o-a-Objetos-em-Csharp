7a75543359a9779079a7b5583993ab8ce0794d08
majulha <majulialcarvalho@gmail.com>
2023-04-07 20:53:54 -0300

    começando herança e polimorfismo

---
+++ 07 - Heritage/Cap1.1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Cap1._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

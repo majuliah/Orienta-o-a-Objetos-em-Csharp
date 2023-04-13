25ffb05af91a7c9536183c4f680ea7eb6f06fddf
majulha <majulialcarvalho@gmail.com>
2023-04-13 19:36:10 -0300

    exception exercice ready

--- 08 - Exception Treatment/Cap1.3/Entities/Account.cs
+++ 08 - Exception Treatment/Cap1.3/Entities/Account.cs
@@ -1,27 +1,42 @@
-﻿namespace Cap1._3.Entities
+﻿using System.Globalization;
+using Cap1._3.Entities.Exceptions;
+
+namespace Cap1._3.Entities
 {
     public class Account
     {
         public int AccountNumber { get; set; }
         public string AccountHolder { get; set; }
         public double AccountBalance { get; set; }
-        public double AccountWithDraw { get; set; }
+        public double AccountWithDrawLimit { get; set; }
         
         public Account(){}
 
-        public Account(int accountNumber, string accountHolder, double accountBalance, double accountWithDraw)
+        public Account(int accountNumber, string accountHolder, double accountBalance, double accountWithDrawLimit)
         {
             AccountNumber = accountNumber;
             AccountHolder = accountHolder;
             AccountBalance = accountBalance;
-            AccountWithDraw = accountWithDraw;
+            AccountWithDrawLimit = accountWithDrawLimit;
         }
 
         public void Deposit(double amount)
         {
+            AccountBalance += amount;
+        }       
+        public void Withdraw(double amount)
+        {
+            if (AccountBalance < amount)
+                throw new BankException($"Balance is less than the amount you want to sack");
+            if (AccountWithDrawLimit < amount)
+                throw new BankException($"Withdraw limit is less than the amount you want to sack");
             
+            AccountBalance -= amount;
         }
-        
-        
+
+        public override string ToString()
+        {
+            return ($"{AccountHolder}, your new amount is ${AccountBalance.ToString($"F2", CultureInfo.InvariantCulture)}");
+        }
     }
 }
--- 08 - Exception Treatment/Cap1.3/Program.cs
+++ 08 - Exception Treatment/Cap1.3/Program.cs
@@ -1,5 +1,9 @@
 ﻿using System;
+using System.Globalization;
+using Cap1._3.Entities;
+using Cap1._3.Entities.Exceptions;
 using static System.Console;
+
 
 namespace Cap1._3
 {
@@ -7,7 +11,35 @@ namespace Cap1._3
     {
         static void Main(string[] args)
         {
-            WriteLine("Hello World!");
+            WriteLine("Enter account data");
+            Write("Number: ");
+            int number = int.Parse(Console.ReadLine());
+            Write("Holder: ");
+            string holder = Console.ReadLine();
+            Write("Initial balance: ");
+            double balance = double.Parse(ReadLine(), CultureInfo.InvariantCulture);
+            Write("Withdraw limit: ");
+            double withdrawLimit = double.Parse(ReadLine(), CultureInfo.InvariantCulture);
+
+            Account account = new Account(number, holder, balance, withdrawLimit);
+            
+            WriteLine($"Enter amount for withdraw: ");
+            balance = double.Parse(ReadLine(), CultureInfo.InvariantCulture);
+
+            try
+            {
+                account.Withdraw(balance);
+                WriteLine(account.ToString());
+            }
+            catch (BankException message)
+            {
+                WriteLine($"Withdraw error. {message.Message}");
+                throw;
+            }
+
+            
+
+
         }
     }
 }

96725a59a830749a1d050fb848b71eae00b87f19
majulha <majulialcarvalho@gmail.com>
2023-04-21 20:30:01 -0300

    exercicio de fixacao

---
+++ 10 - Interfaces/Cap1.2/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using static System.Console;
+
+namespace Cap1._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

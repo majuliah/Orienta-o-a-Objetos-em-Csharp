d66177a3629f17967da1c32f45755aa9a0a66e96
majulha <majulialcarvalho@gmail.com>
2023-04-28 16:44:35 -0300

    um pouco melhor

--- 10 - Interfaces/Cap1.3/Program.cs
+++ 10 - Interfaces/Cap1.3/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Globalization;
 using entities;
+using services;
 using static System.Console;
 
 namespace Cap1._3
@@ -10,14 +11,24 @@ namespace Cap1._3
         static void Main(string[] args)
         {
             WriteLine("Enter rental data: ");
-            WriteLine("Car model: ");
+            Write("Car model: ");
             string carModel = ReadLine();
-            WriteLine($"Pick up (dd/MM/yyyy HH:mm)");
+            Write($"Pick up (dd/MM/yyyy HH:mm)");
             DateTime pickup = DateTime.ParseExact(ReadLine(),"dd/MM/yyyy HH:mm", CultureInfo.InvariantCulture );
-            WriteLine($"Return (dd/MM/yyyy HH:mm)");
+            Write($"Return (dd/MM/yyyy HH:mm)");
             DateTime returned = DateTime.ParseExact(ReadLine(),"dd/MM/yyyy HH:mm", CultureInfo.InvariantCulture );
 
+            Write($"Enter price per hour: ");
+            double priceHour = double.Parse(ReadLine(), CultureInfo.InvariantCulture);
+            Write($"Enter price per day: ");
+            double priceDay = double.Parse(ReadLine(), CultureInfo.InvariantCulture);
+            
             CarRental rental = new CarRental(pickup, returned, new Vehicle(carModel));
+            RentalServices rentalServices = new RentalServices(priceHour, priceDay);
+            rentalServices.ProcessInvoice(rental);
+            
+            WriteLine($"Invoice: ");
+            WriteLine(rental.Invoice);
         }
     }
 }
--- 10 - Interfaces/Cap1.3/Services/RentalServices.cs
+++ 10 - Interfaces/Cap1.3/Services/RentalServices.cs
@@ -1,4 +1,5 @@
-﻿using entities;
+﻿using System;
+using entities;
 
 namespace services
 {
@@ -7,13 +8,30 @@ namespace services
         public double ValuePerHour { get; private set; }
         public double ValuePerDay { get; private set; }
 
+        private BrazilTaxService _brazilTaxService = new BrazilTaxService();
+
         public RentalServices(double valuePerHour, double valuePerDay)
         {
             ValuePerHour = valuePerHour;
             ValuePerDay = valuePerDay;
         }
-        
+
         public void ProcessInvoice(CarRental carRental)
-        {}
+        {
+            TimeSpan duration = carRental.Finish.Subtract(carRental.Start);
+            double basicPayment = 0.0;
+
+            if (duration.TotalHours <= 12)
+            {
+                basicPayment = ValuePerHour * Math.Ceiling(duration.TotalHours);
+            }
+            else
+            {
+                basicPayment = ValuePerDay * Math.Ceiling(duration.TotalDays);
+            }
+            double tax = _brazilTaxService.Tax(basicPayment);
+            
+            carRental.Invoice = new Invoice(basicPayment, tax);
+        }
     }
 }

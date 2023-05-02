0ae044e6b01d885ec2ab12ccca5c6536e053ee61
majulha <majulialcarvalho@gmail.com>
2023-05-02 17:14:44 -0300

    comecando novo capitulo

---
+++ 11 - Generics, Set, Dictionary/Cap1.1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using static System.Console;
+
+namespace Cap1._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Hello World!");
+        }
+    }
+}

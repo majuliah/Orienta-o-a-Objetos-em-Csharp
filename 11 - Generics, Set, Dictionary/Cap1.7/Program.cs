2df56dbf8f195ff392f8157612bc626e909590da
majulha <majulialcarvalho@gmail.com>
2023-05-04 20:11:52 -0300

    exercicios resolvidos

---
+++ 11 - Generics, Set, Dictionary/Cap1.7/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using static System.Console;
+
+namespace Cap1._7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Hello World!");
+        }
+    }
+}

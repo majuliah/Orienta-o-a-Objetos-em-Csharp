5eff291b574c80a62a50e70aec3908240a61b7fd
majulha <majulialcarvalho@gmail.com>
2023-05-09 21:51:24 -0300

    dictionary

---
+++ 11 - Generics, Set, Dictionary/Cap1.7/Relogio.cs
@@ -0,0 +1,32 @@
+﻿namespace Cap1._7
+{
+    
+    class Relogio
+    {
+        int Hora;
+        int Minutos;
+        int Segundos;
+        int Milisegundos;
+
+        int CalculaBatimentosCardiacos()
+        {
+            // operação necessaria para calcular batimentos cardiacos
+            return 0;
+        }
+
+        int CalculaFusoHorario()
+        {
+            // operação necessaria para calcular batimentos cardiacos
+            return 0;
+        }
+
+        string DizerBomDia()
+        {
+            return "Bom dia";
+        }
+
+    }
+    
+    
+}
+

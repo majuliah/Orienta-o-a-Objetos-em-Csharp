a4c80389a5a7e8a7df242e574777672869424396
majulha <majulialcarvalho@gmail.com>
2023-05-16 15:28:29 -0300

    exercice of dictionary done

---
+++ 11 - Generics, Set, Dictionary/Cap1.8/Entities/Candidates.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace entities
+{
+    public class Candidates
+    {
+        public string Name { get; set; }
+        public int Votes { get; set; }
+
+        public override int GetHashCode()
+        {
+            return Name.GetHashCode();
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (!(obj is Candidates))
+            {
+                return false;
+            }
+            else
+            {
+                Candidates candidate = obj as Candidates;
+                return Name.Equals(candidate.Name);
+            }
+        }
+    }
+}
--- 11 - Generics, Set, Dictionary/Cap1.8/Program.cs
+++ 11 - Generics, Set, Dictionary/Cap1.8/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using System.Collections.Generic;
+using System.IO;
+using static System.Console;
 
 namespace Cap1._8
 {
@@ -6,7 +8,37 @@ namespace Cap1._8
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            WriteLine("Enter with the files path");
+            string path = ReadLine();
+
+            try
+            {
+                using (StreamReader streamReader = File.OpenText(path))
+                {
+                    Dictionary<string, int> votesInformation = new Dictionary<string, int>();
+                    while (!streamReader.EndOfStream)
+                    {
+                        string[] line = streamReader.ReadLine().Split(',');
+                        string candidatesName = line[0];
+                        int votes = int.Parse(line[1]);
+
+                        if (votesInformation.ContainsKey(candidatesName))
+                            votesInformation[candidatesName] += votes;
+                        else
+                            votesInformation[candidatesName] = votes;
+                    }
+
+                    foreach (var values in votesInformation)
+                    {
+                        WriteLine($"{values.Key}: {values.Value}");
+                    }
+                }
+            }
+            catch (IOException exception)
+            {
+                WriteLine($"There is something wrong: ");
+                WriteLine(exception.Message);
+            }
         }
     }
 }

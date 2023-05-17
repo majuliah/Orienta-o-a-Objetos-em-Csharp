451639924ec0f1c3e6d988d6b901acdbe3413dec
majulha <majulialcarvalho@gmail.com>
2023-05-17 20:24:09 -0300

    outras formas de executar uma função

--- 13 - Lambda, delegates, LINQ/Cap1.1/Program.cs
+++ 13 - Lambda, delegates, LINQ/Cap1.1/Program.cs
@@ -15,19 +15,30 @@ namespace Cap1._1
             lista.Add(new Product($"TV", 900.00));
             lista.Add(new Product($"Notebook", 1200.00));
             lista.Add(new Product($"Tablet", 450.00));
+
+            //Comparison<Product> comparison = CompareProducts;
+            //lista.Sort(comparison);
+            //a variável comparison guarda a referência para a função
             
-            lista.Sort(CompareProducts);
+            //lista.Sort(CompareProducts);
             // referência para a função, conhecida como delegate
+
+            //Comparison<Product> comparison = (p1, p2) => p1.Name.ToUpper().CompareTo(p2.Name.ToUpper());
+            //expressão lambda que ordena diretamente sem precisar de uma função que faça isso
             
+            lista.Sort((p1, p2) => p1.Name.ToUpper().CompareTo(p2.Name.ToUpper()));
+            //colocando diretamente dentro do sort para não precisar de uma variável intermediária
+
+
             foreach (Product product in lista)
             {
                 WriteLine(product);
             }
         }
 
-        static int CompareProducts(Product p1, Product p2)
-        {
-            return p1.Name.ToUpper().CompareTo(p2.Name.ToUpper());
-        }
+        //static int CompareProducts(Product p1, Product p2)
+        //{
+        //   return p1.Name.ToUpper().CompareTo(p2.Name.ToUpper());
+        //}
     }
 }

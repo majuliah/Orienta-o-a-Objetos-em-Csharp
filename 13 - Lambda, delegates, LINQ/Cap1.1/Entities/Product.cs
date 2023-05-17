4209670d27946fe5b7a27ebd26f18247ebf642a8
majulha <majulialcarvalho@gmail.com>
2023-05-17 20:13:38 -0300

    lista ordenada pelo nome do objeto

---
+++ 13 - Lambda, delegates, LINQ/Cap1.1/Entities/Product.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+
+namespace Cap1._1.Entities
+{
+    public class Product : IComparable<Product>
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public override string ToString()
+        {
+            return ($"{Name}, {Price.ToString($"F2", CultureInfo.InvariantCulture)}");
+        }
+
+        public int CompareTo(Product obj)
+        {
+            return Name.ToUpper().CompareTo(obj.Name.ToUpper());
+        }
+        
+    }
+}
--- 13 - Lambda, delegates, LINQ/Cap1.1/Program.cs
+++ 13 - Lambda, delegates, LINQ/Cap1.1/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using Cap1._1.Entities;
+using static System.Console;
 
 namespace Cap1._1
 {
@@ -6,7 +9,19 @@ namespace Cap1._1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            WriteLine("Começando programação funcional");
+
+            List<Product> lista = new List<Product>();
+            lista.Add(new Product($"TV", 900.00));
+            lista.Add(new Product($"Notebook", 1200.00));
+            lista.Add(new Product($"Tablet", 450.00));
+            
+            lista.Sort();
+            foreach (Product product in lista)
+            {
+                WriteLine(product);
+            }
+
         }
     }
 }

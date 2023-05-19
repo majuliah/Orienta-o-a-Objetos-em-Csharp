9edb8825d9f39d9c6c66f6c64f8e6eb8adcf06cc
majulha <majulialcarvalho@gmail.com>
2023-05-19 18:41:01 -0300

    delegate action

---
+++ 12 - Lambda, delegates, LINQ/Cap1.6/Entities/Product.cs
@@ -0,0 +1,21 @@
+﻿using System.Globalization;
+
+namespace Cap1._6.Entities
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name}, {Price.ToString($"F2", CultureInfo.InvariantCulture)}";
+        }
+    }
+}
--- 12 - Lambda, delegates, LINQ/Cap1.6/Program.cs
+++ 12 - Lambda, delegates, LINQ/Cap1.6/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using Cap1._6.Entities;
 using static System.Console;
 
 namespace Cap1._6
@@ -7,7 +9,39 @@ namespace Cap1._6
     {
         static void Main(string[] args)
         {
-            WriteLine("Hello World!");
+            WriteLine("Fazer um programa que aumente o valor dos produtos em 10%");
+
+            List<Product> list = new List<Product>();
+            
+            list.Add(new Product($"TV", 900.00));
+            list.Add(new Product($"Mouse", 50.00));
+            list.Add(new Product($"Tablet", 350.50));
+            list.Add(new Product($"HD case", 80.90));
+            
+            list.ForEach(UpdtatePrice);
+            foreach (Product p in list)
+            {
+                WriteLine(p);
+            }
+
+            Action<Product> action = UpdtatePrice;
+            list.ForEach(action);
+            //sintaxe alternativa
+            
+            Action<Product> action2 = p => { p.Price += p.Price * 0.10; };
+            //quando uma função é void ela precisa estar entre chaves
+            //sintaxe alternativa
+            
+            list.ForEach(p => { p.Price += p.Price * 0.10; });
+            //sintaxe alternativa utilizando a expressão lambda inline
+            
         }
+
+        static void UpdtatePrice(Product p)
+        {
+            p.Price += p.Price * 0.10;
+        }
+        
+        
     }
 }
